630785bb836b7218fb93478da2db99f6c4a45b2a
Elena Perez <elenpere@c2r11s3.42madrid.com>
2024-11-20 16:11:02 +0100

    ex01 subido desde 42 norminette comprobada

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 15:50:49 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/20 16:10:32 by elenpere         ###   ########.fr       */
+/*   Created: 2024/11/20 16:08:34 by elenpere          #+#    #+#             */
+/*   Updated: 2024/11/20 16:08:37 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
-}
-/*
-int	main(void)
-{
-	char	src[] = "hello";
-	char	dest[10];
-	int	n = 5;
-
-	printf("%s", ft_strncpy(dest, src, n));
-	return (0);
-}*/
