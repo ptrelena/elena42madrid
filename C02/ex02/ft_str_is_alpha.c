d5d7fdf02879fef9713bc78f7726d3d9141834de
Elena Perez <elenpere@c2r11s3.42madrid.com>
2024-11-20 16:36:29 +0100

    ex02 subido desde 42 - no terminado

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -6,8 +6,22 @@
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:08:34 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/20 16:08:37 by elenpere         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:34:10 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_str_is_alpha(char *str)
+{
+	if ("64" < str  < "91" || "96" < str  < "123" || str == "32")
+		return (1);
+	else
+		return (0);
+}
 
+int	main()
+{
+	str [] = "hello";
+
+	ft_str_is_alpha(str);
+	return (void);
+}
