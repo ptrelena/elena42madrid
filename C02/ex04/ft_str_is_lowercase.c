de953f1279796e992395f15116a439f28efd4b6d
ptrelena <165208374+ptrelena@users.noreply.github.com>
2024-11-21 15:58:27 +0100

    ex04 terminado y compilado falta norminette

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] > 'a') && (str[i] < 'z'))
+			return (1);
+		else
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char	str [] = "Hello";
+
+	printf("%d", ft_str_is_lowercase(str));
+	return (0);
+}*/
