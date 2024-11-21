a75f08a4c577e747639a8cd40df013e5b97876ed
ptrelena <165208374+ptrelena@users.noreply.github.com>
2024-11-21 16:39:36 +0100

    ex05 compila pero ejecuta mal

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] > 'A') && (str[i] < 'Z'))
+			return (1);
+		else
+			return (0);
+		i++;
+	}
+	return(1);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char	str [20] = "ELENA";
+	char	str1 [20] = "Elena";
+	char	str2 [20] = "elena";
+	char	str3 [20] = "ELENA1";
+	char	str4 [20] = "123";
+
+	printf("%d", ft_str_is_uppercase(str));
+	printf("%d", ft_str_is_uppercase(str1));
+	printf("%d", ft_str_is_uppercase(str2));
+	printf("%d", ft_str_is_uppercase(str3));
+	printf("%d", ft_str_is_uppercase(str4));
+	return (0);
+}
