ac2f62a2113194305f2dbbd2ec70717c7509d4ad
Elena Perez <elenpere@c2r3s6.42madrid.com>
2024-11-21 10:47:59 +0100

    repositorios y archivos vim .c creados con header 42

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -1,37 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 16:08:34 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/21 10:38:46 by elenpere         ###   ########.fr       */
+/*   Created: 2024/11/21 10:44:34 by elenpere          #+#    #+#             */
+/*   Updated: 2024/11/21 10:44:37 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_alpha(char *str)
-{
-	int	i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			return (1);
-		if (str[i] >= 'a' && str[i] <= 'z')
-			return (1);
-		else
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	main(void)
-{
-	char	str [] = "hello";
-	
-	ft_str_is_alpha(str);
-	return (0);
-}
