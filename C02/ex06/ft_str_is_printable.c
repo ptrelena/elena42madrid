92dc6f953ba6001b3d3b54e039f5a3d62bba5121
ptrelena <165208374+ptrelena@users.noreply.github.com>
2024-11-22 14:56:02 +0100

    ex06 terminado y comprobado

--- C02/ex06/ft_str_is_printable.c
+++ C02/ex06/ft_str_is_printable.c
@@ -12,5 +12,26 @@
 
 int ft_str_is_printable(char *str)
 {
-	
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] <= 31) || (str[i] == 127))
+			return (1);
+		else
+			return (0);
+		i++;
+	}
+	return(1);
 }
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char	str [] = "\n";
+
+	printf("%d", ft_str_is_printable(str));
+	return (0);
+}*/
