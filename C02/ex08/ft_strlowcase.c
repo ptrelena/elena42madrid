7383f6222d584aedbd4e4c3d2168b477a66cc293
Elena Perez <elenpere@c2r3s6.42madrid.com>
2024-11-25 09:16:59 +0100

    ex08 terminado y comprobado

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -6,8 +6,32 @@
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:45:43 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/21 10:45:44 by elenpere         ###   ########.fr       */
+/*   Updated: 2024/11/25 09:16:31 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strlowcase(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 65) && (str[i] <= 90))
+			str[i] = str[i] + 32;
+		else
+			str[i] = str[i];
+		i++;
+	}
+	return (str);
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char	str [] = "HELLO";
+
+	printf("%s", ft_strlowcase(str));
+	return (0);
+}*/
