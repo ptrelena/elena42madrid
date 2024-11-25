6308462f5c70fae173b202e3f07eb13269927ba7
Elena Perez <elenpere@c2r3s6.42madrid.com>
2024-11-25 09:10:27 +0100

    ex07 terminado y comprobado

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -6,8 +6,32 @@
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:45:21 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/21 10:45:24 by elenpere         ###   ########.fr       */
+/*   Updated: 2024/11/25 09:09:48 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strupcase(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != '\n')
+	{
+		if ((str[i] >= 97) && (str[i] <= 121))
+			str[i] = str[i] - 32;
+		else
+			str[i] = str[i];
+		i++;
+	}
+	return (str);
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char	str [] = "hello";
+
+	printf("%s", ft_strupcase(str));
+	return(0);
+}*/
