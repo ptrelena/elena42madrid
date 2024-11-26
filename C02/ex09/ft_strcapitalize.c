f8194252bade89cf136c0c716cdd2ca33b6eb833
Elena Perez <elenpere@c2r7s6.42madrid.com>
2024-11-26 10:07:34 +0100

    ex09 hecho y terminado

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -5,62 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 10:46:03 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/25 12:14:59 by elenpere         ###   ########.fr       */
+/*   Created: 2024/11/26 09:40:12 by elenpere          #+#    #+#             */
+/*   Updated: 2024/11/26 10:07:11 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	sumstr(char	*str)
-{
-	int	i;
-
-	i = 0;
-	str[i + 1] = str [i + 1] - 32;
-	i++;
-}
-
-char	*ft_strlowcase(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{   
-		if ((str[i] >= 65) && (str[i] <= 90))
-			str[i] = str[i] + 32; 
-		else
-			str[i] = str[i];
-		i++;
-	}   
-	return (str);
-}
-
 char	*ft_strcapitalize(char *str)
 {
-	int	i;
+	int i;
+	int	word;
 
-	ft_strlowcase(str);
 	i = 0;
+	word = 1;
 	while (str[i] != '\0')
 	{
-		if (i == 0)
-			str[i] = str [i] - 32;
-		else if ((str[i] >= 32) && (str[i] <= 47))
-					|| ((str[i] >= 32) && (str[i] <= 47))
-					|| ((str[i] >= 58) && (str[i] <= 64))
-					|| ((str[i] >= 91) && (str[i] <= 96))
-					|| ((str[i] >= 123) && (str[i] <= 126))
-			sumstr(str);
+		if ((str[i] >= 'a') && (str[i] <= 'z') && (word == 1))
+			str[i] -= 32;
+		if ((str[i] >= 'A') && (str[i] <= 'Z') && (!word))
+			str[i] += 32;
+		if ((str[i] < '0' || (str[i] > '9' && str[i] < 'A'))
+			|| ((str[i] > 'Z' && str[i] < 'a') || str[i] > 'z'))
+			word = 1;
+		else
+			word = 0;
+		i++;
 	}
 	return (str);
 }
 
+/*
 #include <stdio.h>
 
 int	main()
 {
-	char	str [] = "salut, comment tu vas ? 42mots quarante-deux";
+	char	str [] = "salut, coMMent tu vas ? 42mots quarante-deux; cinquante+et+un";
 
 	printf("%s", ft_strcapitalize(str));
 	return (0);
-}
+}*/
