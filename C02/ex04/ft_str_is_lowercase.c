9e828a543da9e355f40d87125fc3dd6609832e19
Elena Perez <elenpere@c2r6s4.42madrid.com>
2024-11-27 11:20:43 +0100

    Retry1: ex04 revisado y comprobado

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -6,7 +6,7 @@
 /*   By: elenpere <elenpere@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:44:14 by elenpere          #+#    #+#             */
-/*   Updated: 2024/11/26 11:18:48 by elenpere         ###   ########.fr       */
+/*   Updated: 2024/11/27 11:20:29 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int	ft_str_is_lowercase(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= 'a') && (str[i] <= 'z'))
-			return (1);
-		else
+		if (!(str[i] >= 'a') && (str[i] <= 'z'))
 			return (0);
 		i++;
 	}
