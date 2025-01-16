567fb0b049cbcdbca552d8a24519d31df5325595
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-01-16 17:10:31 +0100

    Archivos funciones parte 1 creados + añadidos a lib y makefile

--- COMMON_CORE/libft/libft.h
+++ COMMON_CORE/libft/libft.h
@@ -15,9 +15,11 @@
 # define LIBFT_H
 
 // Llamamos a las librerías necesarias para tus funciones
+#include <unistd.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 // Declaras funciones de los archivos a entregar - parte 1, parte 2, bonus
@@ -25,6 +27,21 @@ int ft_isalpha(int c);
 int ft_isalnum(int c);
 int ft_isdigit(int c);
 int ft_isprint(int c);
+void *ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+size_t  ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t  ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
+int ft_toupper(int c);
+int ft_tolower(int c);
+char    *ft_strchr(const char *s, int c);
+char   *ft_strrchr(const char *s, int c);
+int ft_strcmp(const char *s1, const char *s2);
+void    *ft_memchr(const void *s, int c, size_t n);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+int ft_atoi(const char *str);
+void *ft_calloc(size_t count, size_t size);
 
 // Cerramos el condicional if
 #endif
