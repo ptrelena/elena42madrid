55996ca1ba24c7e633365ffd71755e1ed2940384
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-02-18 16:28:01 +0100

    substr ok 18.02

--- COMMON_CORE/libft/ft_substr.c
+++ COMMON_CORE/libft/ft_substr.c
@@ -9,3 +9,40 @@
 /*   Updated: 2025-02-18 09:03:46 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*r;
+
+	if (!s) //initial str not exists
+		return (NULL);
+	if (start >= ft_strlen(s)) //nowhere to start from
+		return(strdup("")); //create & copy space for NULL
+	if (len > (ft_strlen(s) - start)) //bytes to copy > bytes left to copy
+		len = ft_strlen(s) - start; //copy only what's left to copy
+	r = malloc(sizeof(char) * (len + 1)); //create space for new str + '\0'
+	if (!r)
+		return (NULL);
+	i = 0; //counter to loop new str
+	while (len > i) //loop until end of len is reached
+	{
+		r[i] = s[start]; //copy s in new str from start
+		i++;
+		start++;
+	}
+	r[i] = '\0'; //add NULL to new str end
+	return(r);
+}
+
+// int	main(void)
+// {
+// 	char	a [] = "university"; //*s
+// 	int	b = 3; //start
+// 	size_t	c = 5; //len
+
+// 	printf("%s", ft_substr(a, b, c));
+// 	return(0);
+// }
