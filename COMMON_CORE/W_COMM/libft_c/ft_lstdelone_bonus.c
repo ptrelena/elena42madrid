d01ceaa59333dd1898ae0e3ea412fbbf4968771d
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-02-24 15:43:14 +0100

    lst_delone ok 24.02

--- COMMON_CORE/W_COMM/libft_c/ft_lstdelone_bonus.c
+++ COMMON_CORE/W_COMM/libft_c/ft_lstdelone_bonus.c
@@ -11,3 +11,29 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if(!lst) //nothing to return if empty list
+		return ;
+	del(lst->content); //del content inside lst node
+	free(lst); //free emptied node
+}
+
+/* int main()
+{
+    char *content = malloc(15);
+    if (!content)
+        return (1);
+    
+    t_list *node = ft_lstnew(content);
+    if (!node)
+    {
+        free(content);
+        return (1);
+    }
+
+    ft_lstdelone(node, free);
+
+    return 0;
+} */
