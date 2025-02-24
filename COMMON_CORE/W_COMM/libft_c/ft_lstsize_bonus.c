825dd5b5882a767a67e062717aad4790abc8e89c
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-02-24 13:40:25 +0100

    lstsize ok 24.02

--- COMMON_CORE/W_COMM/libft_c/ft_lstsize_bonus.c
+++ COMMON_CORE/W_COMM/libft_c/ft_lstsize_bonus.c
@@ -11,3 +11,41 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int		ft_lstsize(t_list *lst)
+{
+	int	nodes; //counter for ptr in lst
+
+	nodes = 0;
+	while (lst) //list not NULL
+	{
+		lst = lst->next; //loop to next node
+		nodes++; //++ until NULL is reached
+	}
+	return (nodes); //return num of nodes in lst
+}
+
+/* int main()
+{
+    t_list *node1 = ft_lstnew("first node");
+    t_list *node2 = ft_lstnew("second node");
+    t_list *node3 = ft_lstnew("third node");
+	int	total_nodes;
+
+    //link nodes
+    node1->next = node2;
+    node2->next = node3;
+
+    //call ft
+    total_nodes = ft_lstsize(node1);
+
+    //
+    printf("lst has %d nodes\n", total_nodes);
+
+    //free node space
+    free(node1);
+    free(node2);
+    free(node3);
+
+    return (0);
+} */
