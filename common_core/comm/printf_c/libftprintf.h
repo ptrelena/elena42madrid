60f64933f307dc722ffb12fbf3c3bd7029682aa5
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-03-03 15:46:15 +0100

    printf creado 03.03

---
+++ common_core/comm/printf_c/libftprintf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elenpere <elenpere@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-03-03 14:05:37 by elenpere          #+#    #+#             */
+/*   Updated: 2025-03-03 14:05:37 by elenpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTFT.H
+# define LIBFTPRINTFT.H
+
+# include <ctype.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+// # include <stdarg.h>
+
+//include fts
+
+#endif
---
+++ common_core/comm/printf_c/printf.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elenpere <elenpere@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-03-03 14:03:18 by elenpere          #+#    #+#             */
+/*   Updated: 2025-03-03 14:03:18 by elenpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+char	ft_convert(char n)
+{
+	if (n == c)
+		// imprimir char
+/* 	else if (n == 's')
+		// imprimir str
+	else if (n == 'p')
+		// imprimir ptr
+	else if (n == 'd')
+		// imprimir digito
+	else if (n == 'i')
+		// imprimir int
+	else if (n == 'u')
+		// imprimir unsigned
+	else if (n == 'x')
+		// imprimir hexadegimal minusc = 0123456789abcdef
+	else if (n == 'X')
+		// imprimir hexadecimal mayusc = 0123456789ABCDEF
+	else if (n == '%%')
+		//imprimir %
+	else */
+
+}
+
+int	ft_printf(char const c, ...)
+{
+	int	i;
+	va_list	args;
+
+
+
+
+}
