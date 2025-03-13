0b467ae043ff876890de2ef6c3b572cd39c4efca
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-03-13 15:43:17 +0100

    gnl archivos creados 13.03

--- common_core/comm/get_next_line_c/get_next_line.h
+++ common_core/comm/get_next_line_c/get_next_line.h
@@ -1,16 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elenpere <elenpere@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-03-13 10:31:21 by elenpere          #+#    #+#             */
-/*   Updated: 2025-03-13 10:31:21 by elenpere         ###   ########.fr       */
+/*   Created: 2025-03-13 14:29:12 by elenpere          #+#    #+#             */
+/*   Updated: 2025-03-13 14:29:12 by elenpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_get_next_line(int fd)
-{
-	int	i;
-}
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# include <ctype.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char *ft_get_next_line(int fd);
+
+#endif
