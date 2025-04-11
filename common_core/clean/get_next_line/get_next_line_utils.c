12913455d6ac401a9b4c3facb0afdb27e7fa00db
ptrelena <165208374+ptrelena@users.noreply.github.com>
2025-04-11 17:55:08 +0200

    gnl paco ok, norminette ok 11.04

--- common_core/clean/get_next_line/get_next_line_utils.c
+++ common_core/clean/get_next_line/get_next_line_utils.c
@@ -61,7 +61,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	size_s1 = ft_strlen(s1);
 	size_s2 = ft_strlen(s2);
-	str = malloc(sizeof(char)*(size_s1 + size_s2 + 1));
+	str = malloc(sizeof(char) * (size_s1 + size_s2 + 1));
 	if (str == NULL)
 		return (NULL);
 	i = 0;
